63f56fed9e99d0ac6241ca66f836415fa2d8b017
D0vahzul <78020226+D0vahzul@users.noreply.github.com>
2024-07-26 15:23:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php 
-echo 'enessdsds';
+<!DOCTYPE html>
+<html lang="tr">
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="assets/css/base.css">
+<head>
+    <title>Enes</title>
+</head>
+<body>
+    <h1>enes</h1>
+    
+    <script src="assets/bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
